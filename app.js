bb9c6b3db0ecf31eb120bfa92c495bf3b5d6b754
Claire Rhodes <claire.rhodes13@gmail.com>
2024-10-17 09:45:29 -0700

    first commit

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ let playerLives = 1; // start with 3 lives
 console.log(`Welcome, ${username}! You have ${playerLives} lives to begin your quest.`);
 
 console.log(`You see two paths. The left path leads into a dark and eerie forest. The right path leads to a sunny, open beach.`);
-const firstChoice = prompt(`Which path will you choose?`);
-
+const firstChoice = prompt(`Do you go "right" or "left"?`);
 if(firstChoice === `right`){
     console.log(`After arriving at the beach, you find an angry ogre who wont let you pass.`)
-    const secondChoice = prompt('Do you flee or do you fight?')
+    const secondChoice = prompt('Do you "flee" or do you "fight"?')
     if(secondChoice === `fight`){
         playerlives--
         console.log(`RIP ya died... ggs fam it was nice knowin ya`)
         console.log(`GAME OVER`)
     }
     else{
-        console.log(`You escaped unscathed! Ws in the chat`)
+        console.log(`You ran away (good idea bc the other guy died!) and arrive at boat.`)
+        const thirdChoice = prompt(``)
         if(thirdChoice === `left`){
-            
+
         }
     }
 } else{
